9129426600046e2aed2900a1ccd3fdc348666e93
Ulises <ulipvp@gmail.com>
2026-02-24 22:55:01 -0300

    Desarrollo de ResetPasswordCommand Test

--- Sosa.Gym.API/obj/Debug/net8.0/Sosa.Gym.API.AssemblyInfo.cs
+++ Sosa.Gym.API/obj/Debug/net8.0/Sosa.Gym.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sosa.Gym.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecc59a9d316637aa07105e235acb642fde1f01d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3e3f5dfec09202caab466b612ea20af6f1da0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sosa.Gym.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sosa.Gym.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sosa.Gym.UnitTests/obj/Debug/net8.0/Sosa.Gym.UnitTests.AssemblyInfo.cs
+++ Sosa.Gym.UnitTests/obj/Debug/net8.0/Sosa.Gym.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sosa.Gym.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecc59a9d316637aa07105e235acb642fde1f01d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3e3f5dfec09202caab466b612ea20af6f1da0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sosa.Gym.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sosa.Gym.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
